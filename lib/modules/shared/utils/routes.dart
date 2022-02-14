c6530d6652aca57d65331c7b4eaf1ac79ee5e957
Chaves <pedro.chaves@cittamobi.com.br>
2022-02-14 08:55:41 -0300

    Login e register page refatored

--- lib/modules/shared/utils/routes.dart
+++ lib/modules/shared/utils/routes.dart
@@ -4,7 +4,7 @@ import 'package:barbearia_project/modules/pages/atendimento_page.dart';
 import 'package:barbearia_project/modules/pages/home_page.dart';
 import 'package:barbearia_project/modules/pages/login_page.dart';
 import 'package:barbearia_project/modules/pages/main_page.dart';
-import 'package:barbearia_project/modules/pages/registrer_page.dart';
+import 'package:barbearia_project/modules/pages/register_page.dart';
 import 'package:barbearia_project/modules/pages/selection_page.dart';
 import 'package:barbearia_project/modules/pages/splash_page.dart';
 import 'package:flutter/material.dart';
